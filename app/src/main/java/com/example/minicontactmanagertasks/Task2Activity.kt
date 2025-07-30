e6f46cf98ed5eb76fff643f01ee0313fbec1c9d8
m7mdfox <fcb.mohamed2005@gmail.com>
2025-07-30 20:25:19 +0300

    Task #2 Done

--- app/src/main/java/com/example/minicontactmanagertasks/Task2Activity.kt
+++ app/src/main/java/com/example/minicontactmanagertasks/Task2Activity.kt
@@ -1,6 +1,10 @@
 package com.example.minicontactmanagertasks
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,16 @@ class Task2Activity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val tvName = findViewById<TextView>(R.id.tvName)
+        val etNewName = findViewById<EditText>(R.id.etNewName).text
+        val btnSave = findViewById<Button>(R.id.btnSave)
+        btnSave.setOnClickListener {
+            if (etNewName.isNullOrEmpty()||etNewName.isBlank()){
+                Toast.makeText(this, "Invalid Name", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            tvName.text = etNewName
         }
     }
 }
